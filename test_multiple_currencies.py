c3a6520b6f5e7aeb5641d241d5b9dbd14003c118
Donghui <donghui@petbreeze.co>
2021-10-07 22:15:04 +0900

    Multiple Currency Example: First trial

--- test_multiple_currencies.py
+++ test_multiple_currencies.py
@@ -2,3 +2,12 @@ def test_multiplication():
     dollar_five = Dollar(5)
     dollar_five.times(2)
     assert 10 == dollar_five.amount
+
+
+class Dollar:
+    amount = int
+    def __init__(self, amount: int):
+        pass
+
+    def times(self, multiplier: int):
+        pass
