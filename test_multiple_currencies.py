0d2399b1b5ed4e04ae654dc0e7a7685dffe7c725
Donghui <donghui@petbreeze.co>
2021-10-09 16:20:49 +0900

    Feat: Modfiy times method

--- test_multiple_currencies.py
+++ test_multiple_currencies.py
@@ -1,13 +1,15 @@
 def test_multiplication():
-    dollar_five = Dollar(5)
+    dollar_five: Dollar = Dollar(5)
     dollar_five.times(2)
     assert 10 == dollar_five.amount
+    dollar_five.times(3)
+    assert 15 == dollar_five.amount
 
 
 class Dollar:
     amount = int
     def __init__(self, amount: int):
-        pass
+        self.amount = amount
 
     def times(self, multiplier: int):
-        pass
+        self.amount *= multiplier
