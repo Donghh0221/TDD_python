f6b584c75662946d5bd584cdf4cf8151520de7e5
Donghui <eastshine0221@gmail.com>
2021-10-09 17:46:16 +0900

    Feat: Modify times method for return new object

--- test_multiple_currencies.py
+++ test_multiple_currencies.py
@@ -1,15 +1,16 @@
 def test_multiplication():
-    dollar_five: Dollar = Dollar(5)
-    dollar_five.times(2)
-    assert 10 == dollar_five.amount
-    dollar_five.times(3)
-    assert 15 == dollar_five.amount
+    five: Dollar = Dollar(5)
+    product: Dollar = five.times(2)
+    assert 10 == product.amount
+    product: Dollar = five.times(3)
+    assert 15 == product.amount
 
 
 class Dollar:
     amount = int
+
     def __init__(self, amount: int):
         self.amount = amount
 
     def times(self, multiplier: int):
-        self.amount *= multiplier
+        return Dollar(self.amount * multiplier)
