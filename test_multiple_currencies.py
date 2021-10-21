f06fa3555f46955683e07465bc8b028655db2355
Donghui <eastshine0221@gmail.com>
2021-10-22 08:56:10 +0900

    Test: Add multiplication test for new class Franc

--- test_multiple_currencies.py
+++ test_multiple_currencies.py
@@ -1,25 +1,20 @@
-from typing import ClassVar
+from multiple_currencies import *
 
 
-def test_multiplication():
-    five: Dollar = Dollar(5)
-    assert Dollar(10) == five.times(2)
-    assert Dollar(15) == five.times(3)
+class TestDollar:
+    def test_multiplication(self):
+        five: Dollar = Dollar(5)
+        assert Dollar(10).amount == five.times(2).amount
+        assert Dollar(15).amount == five.times(3).amount
+
+    def test_equality(self):
+        assert Dollar(5).equals(Dollar(5))
+        assert not Dollar(6).equals(Dollar(5))
 
 
-def test_equality():
-    assert Dollar(5).equals(Dollar(5))
-    assert not Dollar(6).equals(Dollar(5))
+class TestFranc:
+    def test_multiplication(self):
+        five: Franc = Franc(5)
+        assert Franc(10).amount == five.times(2).amount
+        assert Franc(15).amount == five.times(3).amount
 
-
-class Dollar:
-    amount = int
-
-    def __init__(self, amount: int):
-        self.amount = amount
-
-    def times(self, multiplier: int) -> ClassVar:
-        return Dollar(self.amount * multiplier)
-
-    def equals(self, object: ClassVar) -> bool:
-        return object.amount == self.amount
